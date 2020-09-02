508e66560bdfc0df30d6c4d7100dd7cc45e99e7b
KLiuli <1498181025@qq.com>
2020-09-02 09:09:57 +0800

    提交代码4

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-        int count = 1 ,m = 7 , n = 5 ;
+        int count = 1, m = 7, n = 5;
         boolean flag = true;
         int square[][] = new int[m][n];
-        for ( int i = 0 ; i< m ; i++ ){
-            for(int j = 0 ; j < n ; j++ ){
-                if ( flag ){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (flag) {
                     square[i][j] = count;
-                }
-                else{
-                    square[i][n-1-j] = count;
+                } else {
+                    square[i][n - 1 - j] = count;
                 }
                 count++;
             }
-            flag=!flag;
+            flag = !flag;
         }
-        for(int j=0;j<n;j++){
-            for(int i=0;i<m;i++){
-                System.out.print(square[i][j]+" ");
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(square[i][j] + " ");
             }
             System.out.println();
         }

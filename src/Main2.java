810407bf25d624a28986b6640d6052e20b34ea8d
KLiuli <1498181025@qq.com>
2020-09-02 09:06:49 +0800

    branch test

--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,16 @@
 public class Main2 {
 
     public static void main(String[] args) {
-        
+        int m = 5, n = 7;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j % 2 == 0) {
+                    System.out.print(i + j * m + " ");
+                } else {
+                    System.out.print(j * m + m + 1 - i + " ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
